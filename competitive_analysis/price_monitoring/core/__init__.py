c49a9f9bf311d158f5a3ba95fa411e5fc60acb42
okoloboga <okoloboga37@gmail.com>
2025-11-07 13:41:42 +0300

    refresh docks, remove old sheets service

--- competitive_analysis/price_monitoring/core/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Основные компоненты модуля мониторинга цен.
-
-Содержит:
-- GoogleSheetsClient: клиент для работы с Google Sheets API
-- PriceMonitor: основная логика мониторинга цен
-- CompetitorAnalyzer: анализ конкурентов
-- NotificationService: сервис уведомлений
-"""
--- competitive_analysis/price_monitoring/templates/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Шаблоны для отчетов и анализа в Google Sheets.
-
-Содержит:
-- PriceReportTemplate: шаблон отчета по ценам
-- CompetitorAnalysisTemplate: шаблон анализа конкурентов
-"""
