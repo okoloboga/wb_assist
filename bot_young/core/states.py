7d8918fc909ad91591515cb276339bf7069890b3
okoloboga <okoloboga37@gmail.com>
2025-11-08 20:36:37 +0300

    young bot works with groups

--- bot_young/core/states.py
+++ bot_young/core/states.py
@@ -6,7 +6,7 @@ from aiogram.fsm.state import State, StatesGroup
 
 class AddChannelStates(StatesGroup):
     """Состояния для добавления канала"""
-    waiting_for_channel = State()  # Ожидание ссылки/username канала
+    waiting_for_channel_input = State()  # Ожидание ссылки/пересланного сообщения для канала
     entering_hours = State()  # Ввод часов (0-23)
     entering_minutes = State()  # Ввод минут (0-59)
 
